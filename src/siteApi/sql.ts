25ba2b2e1713d0de8153a7d2d0ebde38c3146db0
Lawrence <lawrence.raphael.castro.yong@gmail.com>
2018-11-21 10:24:01 -0500

    fixed sql idle timeout

--- src/siteApi/sql.ts
+++ src/siteApi/sql.ts
@@ -2,24 +2,10 @@ import mysql, { Connection, ConnectionConfig } from "mysql";
 import { Env } from "./env";
 
 export class SQL {
-	private connection: Connection;
+	private connection!: Connection;
 
 	constructor() {
-		const config: ConnectionConfig = {host: Env.DB_HOST, user: Env.DB_USER, password: Env.DB_PASS} as ConnectionConfig;
-		this.connection = mysql.createConnection(config);
-		this.connection.connect((err: any) => {
-			if (err) {
-				let errMsg: string = "";
-				if (typeof(err) === "string") {
-					errMsg = err;
-				} else if (err instanceof Error) {
-					errMsg = err.message || err.name;
-				}
-				throw new Error("Failed to connect " + errMsg);
-			} else {
-				console.log("Database Connected");
-			}
-		});
+		this.handleDisconnect();
 	}
 
 	public async query( query: string ) {
@@ -41,6 +27,47 @@ export class SQL {
 				}
 				resolve();
 			});
+		});
+	}
+
+	private handleDisconnect() {
+		const config: ConnectionConfig = {host: Env.DB_HOST, user: Env.DB_USER, password: Env.DB_PASS} as ConnectionConfig;
+		this.connection = mysql.createConnection(config); // Always recreate the connection since the old one is invalid
+
+		this.connection.connect((err: any) => {
+			if (err) {
+				// The server is either down or restarting
+				let errMsg: string = "";
+				if (typeof(err) === "string") {
+					errMsg = err;
+				} else if (err instanceof Error) {
+					errMsg = err.message || err.name;
+				}
+				console.error("Failed to connect " + errMsg);
+
+				/**
+				 * We introduce a delay before attempting to reconnect to avoid a hot loop and to allow our script
+				 * to process async requests in the meantime.
+				 */
+				setTimeout(this.handleDisconnect, 2000);
+			} else {
+				console.log("Database Connected");
+			}
+		});
+
+		this.connection.on("error", (err) => {
+			console.error("MySQL Connection Error has occured.");
+			console.error(err);
+
+			/**
+			 * Connection to the MySQL server is usually lost due to either server restart, or an idle connection
+			 * timeout (the wait_timeout server variable configures this).
+			 */
+			if (err.code === "PROTOCOL_CONNECTION_LOST") {
+				this.handleDisconnect();
+			} else {
+				throw err;
+			}
 		});
 	}
 
